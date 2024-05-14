c09814a285c4cbbaabcfec615a95ca79cb6fe7a4
Rosapedroamaral <166629400+Rosapedroamaral@users.noreply.github.com>
2024-05-14 09:56:13 -0300

    Update descbot_v0_1.py

--- DESCBOT_V_0_1/descbot_v0_1.py
+++ DESCBOT_V_0_1/descbot_v0_1.py
@@ -17,8 +17,12 @@ if "user_key" not in st.session_state:
     st.session_state["user_key"] = ""
   
 st.subheader('Insira seu Documento e sua Key Para inicializar')
-user_key = st.text_input('Digite sua key:', key='chave')
+user_key = st.text_input('Digite sua key:',st.session_state["user_key"])
+submit = st.button("Submit")
 st.session_state["user_key"] = user_key
+if submit:
+    st.session_state["user_key"] = user_key
+
 uploaded_file = st.file_uploader('Envie um documento PDF:', type=['pdf'])
 
 USER = "user"
